95b8b6e8f0ad9ed3668b1bf345263a108783dc8d
Victor Tortolero <victor.tortolero@wizeline.com>
2018-02-27 19:09:29 -0600

    Add fetchRecords lambda

---
+++ functions/read.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+var dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+const defaultResponse = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+module.exports.fetchRecords = (event, context, callback) => {
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE
+  };
+
+  dynamoDB.scan(params, function (err, data) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    const response = Object.assign({}, defaultResponse, {
+      statusCode: '200',
+      body: JSON.stringify({ items: data.Items })
+    });;
+
+    callback(null, response);
+  });
+};
